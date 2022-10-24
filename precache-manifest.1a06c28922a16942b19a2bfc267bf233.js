248bcf9da5b9a69796f78525803b5e82c41f287a
Przemysław <przemekkumor4@gmail.com>
2022-10-24 13:46:49 +0200

    Updates

---
+++ precache-manifest.1a06c28922a16942b19a2bfc267bf233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1f09ee5f5aeaf757dbe",
+    "url": "/restapi/static/css/main.8cdf2e2f.chunk.css"
+  },
+  {
+    "revision": "e1f09ee5f5aeaf757dbe",
+    "url": "/restapi/static/js/main.e1f09ee5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/restapi/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5a83691acc56a0ebe517",
+    "url": "/restapi/static/js/runtime~main.5a83691a.js"
+  },
+  {
+    "revision": "a15342d7ca19e05b792c18869788a3ff",
+    "url": "/restapi/static/media/weather.a15342d7.jpg"
+  },
+  {
+    "revision": "fc799fd660a1636c8d4bcf4a3448dd8a",
+    "url": "/restapi/index.html"
+  }
+];
